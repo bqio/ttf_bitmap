39a5227fc7015ee2c85c1dc028d1df5902917afa
bqio <mihalzov.dima@yandex.ru>
2022-04-14 07:39:03 +0300

    update

--- app.py
+++ app.py
@@ -14,41 +14,46 @@ def get_size(text_string, font):
 
 X_OFFSET = 0
 X_PADDING = 15
-Y_PADDING = 15
+Y_PADDING = 12
 MAX_HEIGHT = 110
 STROKE_WIDTH = 3
 SPACE_WIDTH = 20
+TOP = 0
 
-FONT_NAME = "Neucha-Regular.ttf"
+FONT_NAME = "Roboto-Medium.ttf"
 FONT_SIZE = 80
 GRID_COLOR = "#FF00FF"
 TEXT_COLOR = "#FFFFFF"
 BORDER_COLOR = "#00FFFF"
 
 RANGES = [
-  (0x20, 0x7F),
-  (0x0410, 0x0451)
+  (0x21, 0x7E),
+  (0x0410, 0x044F),
+  (0x0451, 0x0451)
 ]
 
-wrapper = Image.new("RGBA", (15000, MAX_HEIGHT + 1))
+PATCH_FONT = [
+  [0x22, 0, -12],
+  [0x24, 0, -5],
+  [0x25, 0, -14]
+]
+
+wrapper = Image.new("RGB", (15000, MAX_HEIGHT + 1))
 drawer = ImageDraw.Draw(wrapper)
 font = ImageFont.truetype(FONT_NAME, FONT_SIZE)
 
 for _range in RANGES:
   start, end = _range
   for ch in range(start, end + 1):
-    w = 0
-    h = 0
-    dim = get_size(chr(ch), font)
-    if ch == 0x20:
-      drawer.rectangle([X_OFFSET, 0, X_OFFSET + SPACE_WIDTH + (X_PADDING * 2), MAX_HEIGHT], outline=GRID_COLOR)
-      X_OFFSET += SPACE_WIDTH + X_PADDING * 2
-      continue
-    if not dim:
-      ch = 0x0023
-    w, h = get_size(chr(ch), font)
-    drawer.text((X_OFFSET + X_PADDING, 0 + Y_PADDING), chr(ch), font=font, stroke_width=STROKE_WIDTH, stroke_fill=BORDER_COLOR, fill=TEXT_COLOR)
-    drawer.rectangle([X_OFFSET, 0, X_OFFSET + w + (X_PADDING * 2), MAX_HEIGHT], outline=GRID_COLOR)
-    X_OFFSET += w + X_PADDING * 2
+    y = TOP
+    for patch in PATCH_FONT:
+      if ch == patch[0]:
+        y = patch[2]
+    drawer.text((X_OFFSET, y), chr(ch), font=font, stroke_width=STROKE_WIDTH, stroke_fill=BORDER_COLOR, fill=TEXT_COLOR)
+    bbox = drawer.textbbox((X_OFFSET, y), chr(ch), font=font, stroke_width=STROKE_WIDTH)
+    print(bbox)
+    drawer.rectangle([bbox[0], 0, bbox[2], MAX_HEIGHT], outline=GRID_COLOR)
+    X_OFFSET = bbox[2] + STROKE_WIDTH
 
+#wrapper.show()
 wrapper.save("grid.png", "PNG")
